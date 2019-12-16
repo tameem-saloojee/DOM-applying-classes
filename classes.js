9b52ed155b99068bba30981180c222a66b1b95f5
tameem-saloojee <saloojeetameem@gmail.com>
2019-12-17 03:48:27 +1300

    classes

--- classes.js
+++ classes.js
@@ -5,11 +5,25 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
+function start() {
+  two()
+}
+
+
+function start() {
+  three()
+}
+
+function start (){
+
+  makeVisible()
+
+}
 function one () {
   // First, we have to find the element:
   var one = document.getElementById('one')
@@ -19,7 +33,24 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two(){
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
+
 
 // CREATE FUNCTION three HERE
 
+function three(){
+  var three = document.getElementById('three')
+  three.classList.add('red')
+
+
+}
+
 // CREATE FUNCTION makeVisible HERE
+function makeVisible (){
+
+  var m = document.getElementsByClassName('invisible')
+  m[0].classList.add('visible')
+}
